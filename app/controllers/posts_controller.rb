17ada35db47619932758a86bca5580c9c3f9454f
Jameson M. Knoll <j.knoll621@gmail.com>
2014-12-13 02:01:35 +0800

    root to posts/index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate, except: [:index, :show]
+  #before_action :authenticate, except: [:index, :show]
 
   # GET /posts
   # GET /posts.json
